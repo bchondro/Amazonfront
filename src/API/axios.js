ff43d24a56a13b36fcf3d3313e6b6b223f864fe7
AmareGatie <amaregatie2006@gmail.com>
2025-03-11 08:24:24 +0300

    done

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // backend deployed on render
-  // baseURL: "https://amazon-api-deploy-sc1y.onrender.com",
+  baseURL: "https://amazon-api-deploy-sc1y.onrender.com",
   // backend locally using firebase functions
-  baseURL: "http://127.0.0.1:5001/sep-449604/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/sep-449604/us-central1/api",
   // backend locally using express server on port 5000
   // baseURL: "http://localhost:5000",
 });
